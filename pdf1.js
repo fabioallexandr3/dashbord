b36a3bce5eeef6691108da80ee6a978a1cdec532
Fábio Alexandre <fabioallexandr3@hotmail.com>
2023-01-21 17:04:36 -0300

    Adição de um botão

--- pdf1.js
+++ pdf1.js
@@ -11,10 +11,7 @@
 
 function gerarpdf() {
 
-    var pdfMake = require('pdfmake/build/pdfmake.js');
-    var pdfFonts = require('pdfmake/build/vfs_fonts.js');
-    pdfMake.vfs = pdfFonts.pdfMake.vfs;
-    pdfMake.createPdf(docDefinition).download();
+    alert('Olá mundo!')
 
 
 }
