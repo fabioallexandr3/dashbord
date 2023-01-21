fad9946b4170c84825061eaf8a851d877ba90e73
Fábio Alexandre <fabioallexandr3@hotmail.com>
2023-01-21 17:14:25 -0300

    arquivo script.js modificado

--- script.js
+++ script.js
@@ -1,13 +1,4 @@
 
-//   function gerarpdf() {
-
-//     var doc = new jsPDF()
-
-//     doc.text("Olá mundo mais uma vez!", 10, 10)
-//     doc.save("Meu PDF.pdf")
-
-//   }
-
 
 function gerarpdf() {
 
@@ -16,19 +7,3 @@ function gerarpdf() {
 
 }
 
-// function gerarpdf() {
-//     var pdf = new jsPDF();
-
-//     // alert('Olá mundo!')
-//     var elementHTML = document.getElementById('content').innerHTML;
-//     var specialElementHandlers = {
-//         '#elementH': function (element, renderer) {
-//             return true;
-//         }
-//     };
-//     pdf.fromHTML(elementHTML, 15, 15, {
-//         'width': 170,
-//         'elementHandlers': specialElementHandlers
-//     });
-//     pdf.save('sample-file.pdf');
-// }
